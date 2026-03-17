9f491623810e27784e6e17c9b6279d14852e12e0
kary2244 <karyrey52@gmail.com>
2026-03-17 13:36:07 -0600

    arreglo de url imagenes https

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3333/api'
+//export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3333/api'
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (isLocalhost
+	? 'http://localhost:3333/api'
+	: 'https://ruprecht-backend.onrender.com/api');
